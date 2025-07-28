35a3645845fe099a91138651034e9f0c8d1294db
Sandeepbabu0211 <sandeepbabuhn1718@gmail.com>
2025-07-28 14:37:52 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://18.140.255.44:9000/',
+  serverUrl: 'http://18.140.255.44:9000',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'squ_e77a8ed10a28e2796ee1697f57d0766d8a0277e0',
-	     //'sonar.login':'admin',
-	     //'sonar.password':'passw0rd',
+	    //'sonar.login': 'squ_e77a8ed10a28e2796ee1697f57d0766d8a0277e0',
+	     'sonar.login':'admin',
+	     'sonar.password':'passw0rd',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
